0edb7b7131e91fadc2bf2cea90b697f55b6ed6f2
Ryan Mottola <rmottola68@gmail.com>
2022-07-27 02:02:30 -0400

    commit tree 4fbc9fb21a4167566741852636f96edaee4f6c4f

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,9 +15,17 @@ puts "🌱 Seeding data..."
     Review.create(
       score: rand(1..10),
       comment: Faker::Lorem.sentence,
-      game_id: game.id # use the ID (primary key) of the game as the foreign key
+      game_id: game.id, # use the ID (primary key) of the game as the foreign key,
+      user_id: rand(1..5)
     )
   end
+
+  5.times do
+    User.create(
+      name: Faker::Name.name
+    )
+  end
+
 end
 
 puts "🌱 Done seeding!"
